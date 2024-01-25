14558f28f78ad15270e412104281635b1b4b832e
gantoho <i@ganto.icu>
2024-01-25 23:56:29 +0800

    test(style): 调整play.spec.ts单元测试结构

--- src/store/tests/player.spec.ts
+++ src/store/tests/player.spec.ts
@@ -7,43 +7,48 @@ describe('player', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
   })
-  it('should move to top', () => {
-    const { setupMap } = useMapStore()
-    const newMap = [
-      [1, 2, 1],
-      [2, 2, 2],
-      [1, 2, 1],
-    ]
-    setupMap(newMap)
-    const { player, movePlayerToTop } = usePlayerStore()
-    player.x = 1
-    player.y = 1
-    movePlayerToTop()
-    expect(player.y).toBe(0)
-  })
 
-  it('should move to bottom', () => {
-    const { player, movePlayerToBottom } = usePlayerStore()
-    player.x = 1
-    player.y = 1
-    movePlayerToBottom()
-    expect(player.y).toBe(1)
-  })
-
-  it('should move to left', () => {
-    const { player, movePlayerToLeft } = usePlayerStore()
-    player.x = 1
-    player.y = 1
-    movePlayerToLeft()
-    expect(player.x).toBe(1)
-  })
-
-  it('should move to right', () => {
-    const { player, movePlayerToRight } = usePlayerStore()
-    player.x = 1
-    player.y = 1
-    movePlayerToRight()
-    expect(player.x).toBe(2)
+  describe('normal move', () => {
+    beforeEach(() => {
+      const { setupMap } = useMapStore()
+      const newMap = [
+        [1, 2, 1],
+        [2, 2, 2],
+        [1, 2, 1],
+      ]
+      setupMap(newMap)
+    })
+    it('should move to top', () => {
+      const { player, movePlayerToTop } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+      movePlayerToTop()
+      expect(player.y).toBe(0)
+    })
+  
+    it('should move to bottom', () => {
+      const { player, movePlayerToBottom } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+      movePlayerToBottom()
+      expect(player.y).toBe(2)
+    })
+  
+    it('should move to left', () => {
+      const { player, movePlayerToLeft } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+      movePlayerToLeft()
+      expect(player.x).toBe(0)
+    })
+  
+    it('should move to right', () => {
+      const { player, movePlayerToRight } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+      movePlayerToRight()
+      expect(player.x).toBe(2)
+    })
   })
 
   it('should not move to left when collision a wall', () => {
